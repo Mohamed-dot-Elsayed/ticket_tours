41f046385cbd07dc029b50c1c160809abb12789b
Mohamed <Mohamed@DESKTOP-CJQ1NVC>
2025-07-23 12:11:20 +0300

    tour

--- src/routes/admins/tours.ts
+++ src/routes/admins/tours.ts
@@ -16,7 +16,7 @@ router
   .get(catchAsync(getAllTours))
   .post(validate(createTourSchema), catchAsync(createTour));
 
+router.get("/add-date", catchAsync(addData));
 router.route("/:id").get(validate(idParams), catchAsync(getTourById));
 
-router.get("/add-date", catchAsync(addData));
 export default router;
